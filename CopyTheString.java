31db1716e56f0fb67bad36d9079a5ebe9fcd111c
ashwinigv <64947996+ashwinigv@users.noreply.github.com>
2022-12-12 20:49:37 +0530

    Add files via upload

---
+++ CopyTheString.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class CopyTheString {
+    public static void main(String[] args) {
+     String str1="iNeuron Bangalore";//
+     String str2="";
+     for(int i=0;i<str1.length();i++)
+     {
+         str2=str2+str1.charAt(i);
+     }
+        System.out.println(str2);
+
+     String str3=str1.toUpperCase();
+        System.out.println(str3);
+        System.out.println(" ");
+    }
+
+}
---
+++ SumOfTheString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfTheString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+
+       int a=Integer.valueOf(A);
+       int b=Integer.valueOf(B);
+
+
+
+
+    }
+}
